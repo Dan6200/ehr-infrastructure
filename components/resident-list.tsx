8e4346e4a46992b1ea5fff6b6415d2279ca0373a
Daniel Nyong <danielnyong66@gmail.com>
2025-10-03 07:53:57 +0100

    Made updates to the component data. Updated types. Added a loading fallback component for data

--- components/resident-list.tsx
+++ components/resident-list.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { PulsingDiv } from '@/components/ui/pulsing-div'
 import {
   Table,
   TableBody,
@@ -9,7 +10,7 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { auth } from '@/firebase/client/config'
-import { Facility, ResidentData } from '@/types'
+import { ResidentData } from '@/types'
 import { onAuthStateChanged, User } from 'firebase/auth'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
@@ -17,8 +18,11 @@ import { useEffect, useState } from 'react'
 export default function residentDataList({
   residentsData,
 }: {
-  residentsData: ResidentData[]
+  residentsData: ResidentData[] | null
 }) {
+  const [displayData, setDisplayData] = useState<ResidentData[] | null>(
+    residentsData,
+  )
   const [admin, setAdmin] = useState<User | null>(null)
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -27,58 +31,66 @@ export default function residentDataList({
     return () => unsubscribe()
   }, [setAdmin])
 
+  useEffect(() => {
+    if (residentsData === null) setDisplayData(Array(50).fill(null))
+    else setDisplayData(residentsData)
+  }, [residentsData])
+
   return (
     admin && (
       <div className="w-fit rounded-md border-2 mx-auto">
-        <Table className="text-base w-[90vw] md:w-[70vw] lg:w-[60vw]">
-          <TableCaption>All Residents In The Facility.</TableCaption>
+        <Table className="text-base w-[90vw] md:w-[70vw] lg:w-[50vw]">
+          <TableCaption>All Residents.</TableCaption>
           <TableHeader className="bg-foreground/20 font-bold rounded-md">
             <TableRow>
-              <TableHead className="text-center md:w-[5vw]">Keyword</TableHead>
-              <TableHead className="text-center md:w-[5vw]">
-                residentData
+              <TableHead className="text-center w-[1vw]">Room Number</TableHead>
+              <TableHead className="text-center w-[2vw]">Resident</TableHead>
+              <TableHead className="text-center w-[10vw]">
+                Facility Address
               </TableHead>
-              <TableHead className="text-center md:w-[20vw]">Address</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {residentsData?.map(
-              ({
-                document_id,
-                facility_id,
-                encrypted_resident_name,
-                address,
-              }: ResidentData) => {
-                return (
-                  <TableRow key={facility_id}>
-                    <TableCell className="text-center">
-                      <Link
-                        href={`/residents/${document_id}`}
-                        className="w-full block"
-                      >
-                        {document_id}
-                      </Link>
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Link
-                        href={`/residents/${document_id}`}
-                        className="w-full block"
-                      >
-                        {encrypted_resident_name}
-                      </Link>
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Link
-                        href={`/residents/${document_id}`}
-                        className="w-full block"
-                      >
-                        {address}
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                )
-              },
-            )}
+            {displayData?.map((data: ResidentData | null, index) => (
+              <TableRow key={data?.facility_id ?? index}>
+                <TableCell className="text-center">
+                  {data ? (
+                    <Link
+                      href={`/residents/${data.document_id}`}
+                      className="w-full block"
+                    >
+                      {data.roomNo}
+                    </Link>
+                  ) : (
+                    <PulsingDiv className="h-5 w-12 mx-auto" />
+                  )}
+                </TableCell>
+                <TableCell className="text-center">
+                  {data ? (
+                    <Link
+                      href={`/residents/${data.document_id}`}
+                      className="w-full block"
+                    >
+                      {data.resident_name ?? 'Vacant'}
+                    </Link>
+                  ) : (
+                    <PulsingDiv className="h-5 w-32 mx-auto" />
+                  )}
+                </TableCell>
+                <TableCell className="text-center">
+                  {data ? (
+                    <Link
+                      href={`/residents/${data.document_id}`}
+                      className="w-full block"
+                    >
+                      {data.address}
+                    </Link>
+                  ) : (
+                    <PulsingDiv className="h-5 w-48 mx-auto" />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </div>
