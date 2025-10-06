51561454e34e68d659f9df7bf9a8b1a3e139c570
Daniel Nyong <danielnyong66@gmail.com>
2025-10-06 05:02:40 +0100

    updated the styling of component

--- packages/assisted-living/components/go-back-link.tsx
+++ packages/assisted-living/components/go-back-link.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { cn } from '@/lib/utils'
 import { ArrowLeft } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { FC, ReactNode } from 'react'
@@ -21,10 +22,16 @@ export const GoBackLink: FC<GoBackLinkProps> = ({
   if (refresh) router.refresh()
   return (
     <a
-      className={className}
+      className={cn(
+        'cursor-pointer text-blue-500 flex w-full gap-5 sm:gap-5 items-center',
+        className,
+      )}
+      href="#"
       onClick={url ? () => router.push(url) : () => router.back()}
     >
-      <ArrowLeft />
+      <span className="rounded-full bg-blue-700/20 p-1">
+        <ArrowLeft className="text-blue-700" />
+      </span>
       {children}
     </a>
   )
