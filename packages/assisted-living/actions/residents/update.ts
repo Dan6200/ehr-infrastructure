c51f49f76353cac66ed67f68659090220ca416a9
Daniel Nyong <danielnyong66@gmail.com>
2025-10-07 13:33:52 +0100

    Refactored and restructured actions to use the new converter function

---
+++ packages/assisted-living/actions/residents/update.ts
@@ -0,0 +1,35 @@
+'use server'
+import { collectionWrapper } from '@/firebase/firestore'
+import db from '@/firebase/server/config'
+import { Resident, createResidentConverter } from '@/types'
+import { notFound } from 'next/navigation'
+import { verifySessionCookie } from '@/firebase/auth/server/definitions'
+
+export async function updateResident(
+  newResidentData: Resident,
+  documentId: string,
+  idToken: string,
+) {
+  try {
+    const decodedClaims = await verifySessionCookie(idToken)
+    const userRoles: string[] = decodedClaims.claims.roles || []
+
+    await db.runTransaction(async (transaction) => {
+      const residentRef = collectionWrapper('residents')
+        .withConverter(createResidentConverter(userRoles))
+        .doc(documentId)
+      const resSnap = await transaction.get(residentRef)
+      if (!resSnap.exists) throw notFound()
+      transaction.update(residentRef, { ...newResidentData })
+    })
+    return {
+      success: true,
+      message: 'Successfully Updated Resident Information',
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: 'Failed to Update the Resident',
+    }
+  }
+}
