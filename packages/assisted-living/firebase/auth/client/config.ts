e632744dddd977f11e532e6c12f05467b0e04f7c
Daniel Nyong <danielnyong66@gmail.com>
2025-10-10 04:39:28 +0100

    Added service worker registration

--- packages/assisted-living/firebase/auth/client/config.ts
+++ packages/assisted-living/firebase/auth/client/config.ts
@@ -4,25 +4,23 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 import { getAnalytics, isSupported } from 'firebase/analytics'
 
 const appName = 'lean-ehr-assisted-living-client'
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
-}
+import { firebaseConfig } from '../../config'
 
 let databaseId: string | undefined = undefined
 if (process.env.VERCEL_ENV === 'preview') {
   databaseId = 'staging'
 }
 
-if (!getApps().find((app) => app?.name === appName))
-  initializeApp(firebaseConfig, appName)
+// Initialize Firebase client app only in the browser environment
+let clientApp = getApps().find((app) => app?.name === appName)
+if (typeof window !== 'undefined' && !clientApp) {
+  clientApp = initializeApp(firebaseConfig, appName)
+}
+console.log('client app', clientApp)
 
 export const analytics = (async () => {
-  if (await isSupported()) return getAnalytics(getApp(appName))
+  if (typeof window !== 'undefined' && (await isSupported()))
+    return getAnalytics(getApp(appName))
   return null
 })()
 
@@ -40,4 +38,16 @@ if (process.env.NODE_ENV === 'development') {
   connectFirestoreEmulator(db, firestoreHost!, Number(firestorePort!))
   connectAuthEmulator(auth, authHost)
   console.log('Client: Connected to Firestore and Auth emulators!')
+}
+
+// Register the service worker
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/auth-service-workers.js', { scope: '/' })
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope)
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error)
+    })
 }
