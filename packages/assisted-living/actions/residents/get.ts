c473f736be2db08cf2181bd26789ce72276f7974
Daniel Nyong <danielnyong66@gmail.com>
2025-10-11 17:25:20 +0100

    refactored to enable cursor based pagination

--- packages/assisted-living/actions/residents/get.ts
+++ packages/assisted-living/actions/residents/get.ts
@@ -1,27 +1,37 @@
 'use server'
+
+import { getAuthenticatedAppAndClaims } from '@/auth/server/definitions'
 import {
   collectionWrapper,
-  getCount,
-  queryWrapper,
+  docWrapper,
+  getDocWrapper,
   getDocsWrapper,
+  queryWrapper,
 } from '@/firebase/firestore-server'
 import {
-  Resident,
+  EncryptedResident,
   Facility,
-  ResidentSchema,
   FacilitySchema,
+  Resident,
   ResidentDataSchema,
+  ResidentSchema,
 } from '@/types'
 import {
   decryptResidentData,
   getFacilityConverter,
   getResidentConverter,
 } from '@/types/converters'
+import {
+  QueryConstraint,
+  documentId,
+  endBefore,
+  limit as limitQuery,
+  limitToLast,
+  orderBy,
+  startAfter,
+} from 'firebase/firestore'
 import { notFound } from 'next/navigation'
 import { z } from 'zod'
-import { getAuthenticatedAppAndClaims } from '@/auth/server/definitions'
-import { DocumentReference } from 'firebase/firestore'
-import { startAfter, limit as limitQuery } from 'firebase/firestore'
 
 // Use a DTO for resident data
 export async function getResidentData(
@@ -33,100 +43,95 @@ export async function getResidentData(
     const { app, idToken } = authenticatedApp
     const userRoles: string[] = (idToken?.roles as string[]) || []
 
-    const residentsColRef = collectionWrapper(
-      app,
-      'providers/GYRHOME/residents',
-    ).withConverter(getResidentConverter(userRoles))
-    const residentSnap = await residentsColRef.doc(documentId).get()
-    if (!residentSnap.exists) throw notFound()
-    const resident = residentSnap.data()
-    let validatedResident: Resident
-    try {
-      validatedResident = ResidentSchema.parse(resident)
-    } catch (error: any) {
-      throw new Error(
-        'Object is not of type Resident  -- Tag:16: ' + error.message,
+    const residentsColRef = (
+      await collectionWrapper<EncryptedResident>(
+        app,
+        'providers/GYRHOME/residents',
       )
-    }
-    const facilitySnap = await collectionWrapper(
-      app,
-      'providers/GYRHOME/facilities',
-    )
-      .withConverter(facilityConverter)
-      .doc(validatedResident.facility_id)
-      .get()
+    ).withConverter(await getResidentConverter())
+    const residentDocRef = await docWrapper(residentsColRef, documentId)
+    const residentSnap = await getDocWrapper(residentDocRef)
 
-    if (!facilitySnap.exists)
+    if (!residentSnap.exists()) throw notFound()
+
+    const resident = await decryptResidentData(residentSnap.data(), userRoles)
+
+    const validatedResident = ResidentSchema.parse(resident)
+
+    const facilitiesColRef = (
+      await collectionWrapper<Facility>(app, 'providers/GYRHOME/facilities')
+    ).withConverter(await getFacilityConverter())
+    const facilityDocRef = await docWrapper(
+      facilitiesColRef,
+      validatedResident.facility_id,
+    )
+    const facilitySnap = await getDocWrapper(facilityDocRef)
+
+    if (!facilitySnap.exists()) {
       throw new Error('Could not find linked facility for this resident')
+    }
     const { address } = facilitySnap.data()
 
-    console.log(validatedResident)
-    // return { ...validatedResident, id: residentSnap.id, address }
-    return null
+    return { ...validatedResident, id: residentSnap.id, address }
   } catch (error: any) {
-    throw new Error('Failed to fetch resident.\n\t\t' + error)
+    throw new Error(`Failed to fetch resident: ${error.message}`)
   }
 }
 
-export async function getResidents() {
+export async function getResidents(): Promise<Resident[]> {
   try {
     const authenticatedApp = await getAuthenticatedAppAndClaims()
     if (!authenticatedApp) throw new Error('Failed to authenticate session')
     const { app, idToken } = authenticatedApp
     const userRoles: string[] = (idToken.claims?.roles as string[]) || []
 
-    const residentsCollection = collectionWrapper(app, 'residents')
-    const residentsSnap = await residentsCollection.get()
-    return residentsSnap.docs.map((doc) => {
-      const resident = doc.data()
-      let validatedResident: Resident
-      try {
-        validatedResident = ResidentSchema.parse(resident)
-      } catch (error: any) {
-        throw new Error(
-          'Object is not of type Resident  -- Tag:20: ' + error.message,
-        )
-      }
-      return validatedResident
-    })
-  } catch (error) {
-    throw new Error('Failed to fetch All Residents Data.\n\t\t' + error)
+    const residentsCollection = (
+      await collectionWrapper<EncryptedResident>(app, 'residents')
+    ).withConverter(await getResidentConverter())
+    const residentsSnap = await getDocsWrapper(residentsCollection)
+
+    return Promise.all(
+      residentsSnap.docs.map(async (doc) => {
+        const resident = await decryptResidentData(doc.data(), userRoles)
+        return ResidentSchema.parse(resident)
+      }),
+    )
+  } catch (error: any) {
+    throw new Error(`Failed to fetch all residents data: ${error.message}`)
   }
 }
 
-export async function getAllFacilities() {
+export async function getAllFacilities(): Promise<Facility[]> {
   try {
     const authenticatedApp = await getAuthenticatedAppAndClaims()
     if (!authenticatedApp) throw new Error('Failed to authenticate session')
     const { app } = authenticatedApp
 
-    const facilitiesCollection = collectionWrapper(
-      app,
-      'providers/GYRHOME/facilities',
-    )
-    const facilitiesSnap = await facilitiesCollection.get()
-    if (!facilitiesSnap.size) throw notFound()
+    const facilitiesCollection = (
+      await collectionWrapper<Facility>(app, 'providers/GYRHOME/facilities')
+    ).withConverter(await getFacilityConverter())
+    const facilitiesSnap = await getDocsWrapper(facilitiesCollection)
+
+    if (facilitiesSnap.empty) throw notFound()
+
     return facilitiesSnap.docs.map((doc) => {
       const facility = { document_id: doc.id, ...doc.data() }
-      let validatedFacility: Facility
-      try {
-        validatedFacility = FacilitySchema.parse(facility)
-      } catch (error: any) {
-        throw new Error(
-          'Object is not of type Facility  -- Tag:19: ' + error.message,
-        )
-      }
-      return validatedFacility
+      return FacilitySchema.parse(facility)
     })
-  } catch (error) {
-    throw new Error('Failed to fetch All Facilities.\n\t\t' + error)
+  } catch (error: any) {
+    throw new Error(`Failed to fetch all facilities: ${error.message}`)
   }
 }
 
-export async function getAllResidentsData(
-  lastDoc?: DocumentReference | number,
-  limit?: number,
-) {
+export async function getAllResidentsData({
+  limit = 25,
+  nextCursorId,
+  prevCursorId,
+}: {
+  limit?: number
+  nextCursorId?: string
+  prevCursorId?: string
+}) {
   try {
     const authenticatedApp = await getAuthenticatedAppAndClaims()
     if (!authenticatedApp) throw new Error('Failed to authenticate session')
@@ -134,28 +139,70 @@ export async function getAllResidentsData(
     const userRoles: string[] = (idToken?.roles as string[]) || []
 
     const residentsCollection = (
-      await collectionWrapper<Resident>(app, `providers/GYRHOME/residents`)
+      await collectionWrapper<EncryptedResident>(
+        app,
+        `providers/GYRHOME/residents`,
+      )
     ).withConverter(await getResidentConverter())
 
-    // 1. Get the total count of residents
-    const countSnapshot = await getCount(residentsCollection)
-    const total = countSnapshot.data().count
+    const constraints: QueryConstraint[] = [orderBy(documentId())]
+    let isPrev = false
 
-    // 2. Get the paginated documents if pagination arguments are provided
-    let collectionQuery = await queryWrapper(
+    if (nextCursorId) {
+      const cursorDocRef = await docWrapper(residentsCollection, nextCursorId)
+      const cursorDoc = await getDocWrapper(cursorDocRef)
+      if (cursorDoc.exists()) {
+        constraints.push(startAfter(cursorDoc))
+      }
+    } else if (prevCursorId) {
+      isPrev = true
+      const cursorDocRef = await docWrapper(residentsCollection, prevCursorId)
+      const cursorDoc = await getDocWrapper(cursorDocRef)
+      if (cursorDoc.exists()) {
+        constraints.push(endBefore(cursorDoc))
+        constraints.push(limitToLast(limit + 1)) // Fetch one extra to check for previous page
+      }
+    }
+
+    if (!isPrev) {
+      constraints.push(limitQuery(limit + 1)) // Fetch one extra to check for next page
+    }
+
+    const collectionQuery = await queryWrapper(
       residentsCollection,
-      // limit ? limitQuery(limit) : (undefined as any),
-      // lastDoc ? startAfter(lastDoc) : (undefined as any),
+      ...constraints,
     )
     const residentsSnapshot = await getDocsWrapper(collectionQuery)
-    const residentsForPage = await Promise.all(
+
+    let residentsForPage = await Promise.all(
       residentsSnapshot.docs.map(async (doc) => ({
         id: doc.id,
         ...(await decryptResidentData(doc.data() as any, userRoles)),
       })),
     )
 
-    // 3. Fetch facility data only for the residents on the current page
+    let hasNextPage = false
+    let hasPrevPage = false
+
+    if (isPrev) {
+      hasPrevPage = residentsForPage.length > limit
+      if (hasPrevPage) {
+        residentsForPage.shift() // Remove the extra item from the beginning
+      }
+      hasNextPage = true
+    } else {
+      hasNextPage = residentsForPage.length > limit
+      if (hasNextPage) {
+        residentsForPage.pop() // Remove the extra item from the end
+      }
+      hasPrevPage = !!nextCursorId
+    }
+
+    const nextCursor = hasNextPage
+      ? residentsForPage[residentsForPage.length - 1]?.id
+      : undefined
+    const prevCursor = hasPrevPage ? residentsForPage[0]?.id : undefined
+
     const facilitiesCollection = (
       await collectionWrapper<Facility>(app, 'providers/GYRHOME/facilities')
     ).withConverter(await getFacilityConverter())
@@ -169,15 +216,20 @@ export async function getAllResidentsData(
         {},
       )
 
-    // 4. Join the data
     const residentsWithAddress = residentsForPage.map((resident) => {
       const address =
         facility_lookup[resident.facility_id] || 'Address not found'
       return { ...resident, address }
     })
 
-    return { residents: residentsWithAddress, total }
+    return {
+      residents: residentsWithAddress,
+      nextCursor,
+      prevCursor,
+      hasNextPage,
+      hasPrevPage,
+    }
   } catch (error: any) {
-    throw new Error('Failed to fetch All Residents Data:\n\t\t' + error)
+    throw new Error(`Failed to fetch all residents data: ${error.message}`)
   }
 }
