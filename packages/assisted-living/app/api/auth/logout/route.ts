30a99504bc0f75c196c791ee5b1c1b36bbc9a200
Daniel Nyong <danielnyong66@gmail.com>
2025-10-12 12:13:19 +0100

    adding logging to test auth flow

--- packages/assisted-living/app/api/auth/logout/route.ts
+++ packages/assisted-living/app/api/auth/logout/route.ts
@@ -4,16 +4,18 @@ import {
   deleteSessionCookie,
   getSessionCookie,
   getVerifiedSessionCookie,
-} from '@/firebase/auth/server/definitions'
+} from '@/auth/server/definitions'
 
 // The endpoint the client calls to sign out and clear the session.
 export async function POST(_request: NextRequest) {
   try {
+    // FOR SERVICE-WORKER AUTH FLOW, THERE SHOULD BE NO SESSION COOKIE SET!
     if (!(await getSessionCookie()))
       return NextResponse.json(
         { message: 'Signed out successfully' },
         { status: 200 },
       )
+    console.log('clearing session cookies...')
     // 2. Decode the session cookie to get the UID
     // Use checkRevoked: true to ensure the cookie hasn't been manually revoked already
     const result = await getVerifiedSessionCookie()
