282c6e845f2a164160d818f71f2a932d43371d2c
Daniel Nyong <danielnyong66@gmail.com>
2025-10-16 15:55:41 +0100

    Added links to every cell

--- packages/assisted-living/components/dashboard/residents-table.tsx
+++ packages/assisted-living/components/dashboard/residents-table.tsx
@@ -25,22 +25,77 @@ export function ResidentsTable({ data }: { data: ResidentData[] }) {
     {
       accessorKey: 'facility_id',
       header: 'Facility ID',
+      cell: ({ row }) => {
+        const resident = row.original
+        return (
+          <Link
+            href={`/admin/dashboard/residents/${resident.id}`}
+            className="w-full block"
+          >
+            {resident.facility_id}
+          </Link>
+        )
+      },
     },
     {
       accessorKey: 'room_no',
       header: 'Room No.',
+      cell: ({ row }) => {
+        const resident = row.original
+        return (
+          <Link
+            href={`/admin/dashboard/residents/${resident.id}`}
+            className="w-full block"
+          >
+            {resident.room_no}
+          </Link>
+        )
+      },
     },
     {
       accessorKey: 'dob',
       header: 'Date of Birth',
+      cell: ({ row }) => {
+        const resident = row.original
+        return (
+          <Link
+            href={`/admin/dashboard/residents/${resident.id}`}
+            className="w-full block"
+          >
+            {resident.dob}
+          </Link>
+        )
+      },
     },
     {
       accessorKey: 'pcp',
       header: 'PCP',
+      cell: ({ row }) => {
+        const resident = row.original
+        return (
+          <Link
+            href={`/admin/dashboard/residents/${resident.id}`}
+            className="w-full block"
+          >
+            {resident.pcp}
+          </Link>
+        )
+      },
     },
     {
       accessorKey: 'address',
       header: 'Address',
+      cell: ({ row }) => {
+        const resident = row.original
+        return (
+          <Link
+            href={`/admin/dashboard/residents/${resident.id}`}
+            className="w-full block"
+          >
+            {resident.address}
+          </Link>
+        )
+      },
     },
   ]
 
