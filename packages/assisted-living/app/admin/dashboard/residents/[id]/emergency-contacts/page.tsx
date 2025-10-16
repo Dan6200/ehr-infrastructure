51fd08525bae3a2728d48d33a0d8f2c86ca8be5f
Daniel Nyong <danielnyong66@gmail.com>
2025-10-16 14:23:26 +0100

    Added a modal dialog for editing

--- packages/assisted-living/app/admin/dashboard/residents/[id]/emergency-contacts/page.tsx
+++ packages/assisted-living/app/admin/dashboard/residents/[id]/emergency-contacts/page.tsx
@@ -1,13 +1,14 @@
 import { notFound, redirect } from 'next/navigation'
 import { getResidentData } from '@/actions/residents/get'
 import EmergencyContacts from '@/components/emergency-contacts'
+import { EditEmergencyContactsDialog } from '@/components/dashboard/residents/edit-emergency-contacts-dialog'
 
 export default async function EmergencyContactsPage({
   params,
 }: {
-  params: Promise<{ id: string }>
+  params: { id: string }
 }) {
-  const { id } = await params
+  const { id } = params
   const residentData = await getResidentData(id).catch((e) => {
     if (e.message.match(/not_found/i)) notFound()
     if (e.message.match(/insufficient permissions/)) redirect('/admin/sign-in')
@@ -16,5 +17,16 @@ export default async function EmergencyContactsPage({
     )
   })
 
-  return <EmergencyContacts contacts={residentData.emergency_contacts} />
+  return (
+    <div className="space-y-8">
+      <div className="md:col-span-2 flex justify-between items-center border-b pb-2 mb-8">
+        <h2 className="text-xl font-semibold">Emergency Contacts</h2>
+        <EditEmergencyContactsDialog
+          documentId={id}
+          contacts={residentData.emergency_contacts}
+        />
+      </div>
+      <EmergencyContacts contacts={residentData.emergency_contacts} />
+    </div>
+  )
 }
