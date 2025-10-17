50d127c572939e2ad6c1837f1daf26daba5f5b91
Daniel Nyong <danielnyong66@gmail.com>
2025-10-17 16:40:18 +0100

    Added select fields for relationships

--- packages/assisted-living/components/residents/form/EmergencyContactBlock.tsx
+++ packages/assisted-living/components/residents/form/EmergencyContactBlock.tsx
@@ -1,37 +1,47 @@
 'use client'
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, Controller } from 'react-hook-form'
 import { Edit, Lock, Trash2 } from 'lucide-react'
+import { useState } from 'react'
 
 import { EditableFormField } from './EditableFormField'
-import { useState } from 'react'
+import { LegalRelationshipEnum, PersonalRelationshipEnum } from '@/types'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Badge } from '@/components/ui/badge'
 
 interface EmergencyContactBlockProps {
   index: number
-  onDelete: (index: number) => void // Callback to remove this contact
+  onDelete: (index: number) => void
 }
 
 export function EmergencyContactBlock({
   index,
   onDelete,
 }: EmergencyContactBlockProps) {
-  const { getValues } = useFormContext()
+  const { getValues, control } = useFormContext()
 
   const initialIsEmContactBlockEditing = () => {
     const contactPath = `emergency_contacts.${index}`
     const contact = getValues(contactPath)
-
     return (
       !contact?.contact_name &&
       !contact?.cell_phone &&
       !contact?.home_phone &&
       !contact?.work_phone &&
-      !contact?.relationship
+      !contact?.legal_relationships?.length &&
+      !contact?.personal_relationships?.length
     )
   }
 
   const [isEmContactBlockEditing, setIsEmContactBlockEditing] = useState(
     initialIsEmContactBlockEditing,
   )
+
   return (
     <div className="mb-8 border-b py-4">
       <h3 className="font-semibold mb-8 flex items-center justify-between">
@@ -61,13 +71,73 @@ export function EmergencyContactBlock({
           isInputDisabled={!isEmContactBlockEditing}
           showLocalEditingControls={false}
         />
-        <EditableFormField
-          name={`emergency_contacts.${index}.relationship`}
-          label="Relationship"
-          description="Emergency Contact's Relationship"
-          isInputDisabled={!isEmContactBlockEditing}
-          showLocalEditingControls={false}
+
+        <Controller
+          control={control}
+          name={`emergency_contacts.${index}.personal_relationships`}
+          render={({ field }) => (
+            <EditableFormField
+              name={field.name}
+              label="Personal Relationship"
+              description="Select the personal relationship(s)."
+              isInputDisabled={!isEmContactBlockEditing}
+              showLocalEditingControls={false}
+              renderInput={(fieldProps) => (
+                <Select
+                  {...fieldProps}
+                  multiple
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger disabled={!isEmContactBlockEditing}>
+                    <SelectValue placeholder="Select personal relationships..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {PersonalRelationshipEnum.options.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option.replace(/_/g, ' ')}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          )}
         />
+
+        <Controller
+          control={control}
+          name={`emergency_contacts.${index}.legal_relationships`}
+          render={({ field }) => (
+            <EditableFormField
+              name={field.name}
+              label="Legal Relationship"
+              description="Select the legal relationship(s)."
+              isInputDisabled={!isEmContactBlockEditing}
+              showLocalEditingControls={false}
+              renderInput={(fieldProps) => (
+                <Select
+                  {...fieldProps}
+                  multiple
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger disabled={!isEmContactBlockEditing}>
+                    <SelectValue placeholder="Select legal relationships..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {LegalRelationshipEnum.options.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option.replace(/_/g, ' ')}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          )}
+        />
+
         <EditableFormField
           name={`emergency_contacts.${index}.cell_phone`}
           label="Cell Phone"
