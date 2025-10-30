9bd3b5c79b63f11281983502ffba4291438a67f8
Daniel Nyong <danielnyong66@gmail.com>
2025-10-31 00:09:33 +0100

    Fixed wrong typing

--- packages/assisted-living/app/admin/dashboard/residents/[id]/page.tsx
+++ packages/assisted-living/app/admin/dashboard/residents/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation'
 
-export default function ResidentPage({ params }: { params: { id: string } }) {
-  redirect(`/admin/dashboard/residents/${params.id}/information`)
+export default async function ResidentPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  redirect(`/admin/dashboard/residents/${(await params).id}/information`)
 }
