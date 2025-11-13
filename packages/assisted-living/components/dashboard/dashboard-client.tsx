95f5e2d2a6bd338afb68697c6ed86b4818beaf01
Daniel Nyong <danielnyong66@gmail.com>
2025-11-13 10:51:05 +0100

    Added resident data for charting

--- packages/assisted-living/components/dashboard/dashboard-client.tsx
+++ packages/assisted-living/components/dashboard/dashboard-client.tsx
@@ -4,17 +4,24 @@ import * as React from 'react'
 import { ChartAreaInteractive } from '@/components/dashboard/chart-area-interactive'
 import { SectionCards } from '@/components/dashboard/section-cards'
 import { FormattedChartData } from '@/app/admin/dashboard/page'
+import { Resident } from '@/types'
 
 export function DashboardClient({
   chartData,
+  residents,
 }: {
   chartData: FormattedChartData
+  residents: Resident[]
 }) {
   const [timeRange, setTimeRange] = React.useState('90d')
 
   return (
     <div className="flex flex-1 flex-col gap-4 md:gap-6">
-      <SectionCards chartData={chartData} timeRange={timeRange} />
+      <SectionCards
+        chartData={chartData}
+        timeRange={timeRange}
+        residents={residents}
+      />
       <div className="@5xl/main:col-span-2">
         <ChartAreaInteractive
           chartData={chartData}
