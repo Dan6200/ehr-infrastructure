ca1a86468c3700ea2c833e35a470fafbe07af72e
Daniel Nyong <danielnyong66@gmail.com>
2025-11-15 21:15:47 +0100

    Made script a module

--- packages/assisted-living/demo-data/seed-database.sh
+++ packages/assisted-living/demo-data/seed-database.sh
@@ -31,9 +31,10 @@ else
   ARGS="-k $KEY_PATH $ARGS"
 fi
 
-# --- Step 0: Create Admin User ---
-echo "--- Step 0: Creating Admin User... ---"
-node dev-utils/create-admin-user.ts dev@mail.com Developer ADMIN,CLINICIAN,CAREGIVER,VIEWER
+# # --- Step 0: Create Admin User ---
+# echo "--- Step 0: Creating Admin User... ---"
+# cd dev-utils/create-user.ts 
+# npm start dev@mail.com Developer ADMIN,CLINICIAN,CAREGIVER,VIEWER
 
 # --- Step 1: Generate Plaintext Demo Data ---
 echo "--- Step 1: Generating all plaintext demo data... ---"
