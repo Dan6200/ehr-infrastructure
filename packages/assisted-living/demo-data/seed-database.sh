fab951c35c721f00b1021eafc96538578dfecf2b
Daniel Nyong <danielnyong66@gmail.com>
2025-11-19 16:29:10 +0100

    still debugging import path aliases

--- packages/assisted-living/demo-data/seed-database.sh
+++ packages/assisted-living/demo-data/seed-database.sh
@@ -71,6 +71,8 @@ set -e # Exit immediately if a command exits with a non-zero status.
 # --- Step 5: Backfill Existing Data to BigQuery ---
 echo "--- Step 5: Backfilling existing Firestore data to BigQuery... ---"
 cd functions/
+echo "pwd"
+pwd
 echo "ls lib/"
 ls lib/
 echo "ls lib/src/"
